76568cd2a2860ce224bc2e58abd5caf7a6bf42c9
Martin Morset <mmo@one.com>
2022-06-08 11:18:44 +0200

    26. Exercise: Looping

--- src/lectures/exercise/loops/loops.go
+++ src/lectures/exercise/loops/loops.go
@@ -15,4 +15,16 @@ package main
 import "fmt"
 
 func main() {
+	for i := 1; i <= 50; i++ {
+		fizz := (i%3 == 0)
+		buzz := (i%5 == 0)
+
+		if fizz && buzz {
+			fmt.Println("FizzBuzz")
+		} else if fizz {
+			fmt.Println("Fizz")
+		} else if buzz {
+			fmt.Println("Buzz")
+		}
+	}
 }
