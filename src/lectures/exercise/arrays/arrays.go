ce899b1842340b06618ff1d3868e8c81774e22f2
Martin Morset <mmo@one.com>
2022-06-08 13:56:46 +0200

    34. Exercise: Arrays

--- src/lectures/exercise/arrays/arrays.go
+++ src/lectures/exercise/arrays/arrays.go
@@ -18,6 +18,32 @@ package main
 
 import "fmt"
 
+type Product struct {
+	name  string
+	price float64
+}
+
+func printShoppingList(shoppingList []Product) {
+	var total float64
+	fmt.Println("The last item is ", shoppingList[len(shoppingList)-1].name)
+	fmt.Printf("There are %d items\n", len(shoppingList))
+	for i := 0; i < len(shoppingList); i++ {
+		total += shoppingList[i].price
+	}
+	fmt.Println("The total price is: ", total)
+}
+
 func main() {
 
+	var shoppingList [4]Product
+
+	shoppingList[0] = Product{name: "Oatmeal", price: 12.70}
+	shoppingList[1] = Product{name: "Bananas", price: 6.90}
+	shoppingList[2] = Product{name: "Mangoes", price: 30.10}
+
+	printShoppingList(shoppingList[:])
+
+	shoppingList[3] = Product{name: "Orange Juice", price: 15.49}
+
+	printShoppingList(shoppingList[:])
 }
