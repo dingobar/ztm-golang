6f72dd09187f785cad75b10b774c014ca0635d11
Martin Morset <mmo@one.com>
2022-06-10 09:45:50 +0200

    86. Demo: WaitGroups

--- src/lectures/demo/waitgroups/waitgroups.go
+++ src/lectures/demo/waitgroups/waitgroups.go
@@ -1,11 +1,32 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "sync"
-    "time"
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	var wg sync.WaitGroup
+	counter := 0
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		counter++
+		go func() {
+			defer func() {
+				fmt.Println(counter, "goroutines remaining")
+				counter--
+				wg.Done()
+			}()
+			duration := time.Duration(rand.Intn(1000) * int(time.Millisecond))
+			fmt.Println("waiting", duration, "milliseconds")
+			time.Sleep(duration)
+		}()
+	}
+	fmt.Println("WAITING FOR GOROUTINES TO FINISH")
+	wg.Wait() // This is equivalent to await
+	fmt.Println("DONE :)")
 }
