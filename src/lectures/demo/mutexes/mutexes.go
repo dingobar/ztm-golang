626cde46978db0827f2ca058014cbb5a8e3bbe81
Martin Morset <mmo@one.com>
2022-06-10 09:46:24 +0200

    87. Demo: Mutexes

--- src/lectures/demo/mutexes/mutexes.go
+++ src/lectures/demo/mutexes/mutexes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -10,6 +11,38 @@ func wait() {
 	time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
 }
 
-func main() {
+type Hits struct {
+	count int
+	sync.Mutex
+}
+
+func serve(wg *sync.WaitGroup, hits *Hits, iteration int) {
+	wait()
+	hits.Lock()
+	defer hits.Unlock()
+	defer wg.Done()
+	hits.count++
+	fmt.Println("Serverd iteration", iteration)
+}
+
+func init() {
 	rand.Seed(time.Now().UnixNano())
+}
+
+func main() {
+	var wg sync.WaitGroup
+	hitCounter := Hits{}
+	for i := 0; i < 2000; i++ {
+		iteration := i
+		wg.Add(1)
+		go serve(&wg, &hitCounter, iteration)
+	}
+
+	fmt.Print("waiting for goroutines...\n\n")
+	wg.Wait()
+	hitCounter.Lock() // The mutex seems to serve no purpose here
+	totalHits := hitCounter.count
+	hitCounter.Unlock()
+
+	fmt.Println("Total hits:", totalHits)
 }
